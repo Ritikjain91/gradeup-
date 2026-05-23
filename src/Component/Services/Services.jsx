cd75ac454a631954fefdba6ec876cd312f190f01
Sashakt <saskhatenterprises@gmail.com>
2026-05-23 19:52:15 +0530

    update the code

--- src/Component/Services/Services.jsx
+++ src/Component/Services/Services.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Services = () => {
   return (
-    <div>
-        <h1>Javascript Mastery Short Notes </h1>
-        <p>We offer a range of services to help you prepare for your interviews, including:</p>
+    <div className="p-6">
+      <p className="text-5xl text-center font-bold">
+        JavaScript Mastery Short Notes
+      </p>
+      <p className="text-lg text-black mt-9 text-center">
+  JavaScript Short Notes for Interview Preparation. These notes are designed to help developers quickly revise important JavaScript concepts commonly asked in interviews. Topics include variables, data types, functions, arrays, objects, DOM manipulation, event handling, promises, async/await, closures, hoisting, scope, debouncing, throttling, event loop, callbacks, ES6 features, and more. Along with short notes, this section also includes frequently asked JavaScript interview questions and answers to help beginners and experienced developers prepare effectively for technical interviews.
+</p>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
