7176f6c8c3c4d4f1ec1a81f50f65c3fe7969e05c
Sashakt <saskhatenterprises@gmail.com>
2026-05-23 19:39:00 +0530

    update the code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Navbar from './Component/Navbar/Navbar';
 import Home from './Component/Home/Home.jsx';
 import About from './Component/About/About.jsx';
+import Services from './Component/Services/Services.jsx';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
         {/* ... */}
       </Routes>
     </BrowserRouter>
---
+++ src/Component/Services/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+        <h1>Javascript Mastery Short Notes </h1>
+        <p>We offer a range of services to help you prepare for your interviews, including:</p>
+    </div>
+  )
+}
+
+export default Services
